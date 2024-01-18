1bb9de432e3e08468438c8276e83cdfb7ff5b285
eze110011 <ezequielmacchi@MacBook-Pro-de-Ezequiel-2.local>
2024-01-18 17:33:27 +0100

    varios cambios

--- script/function_articulos.js
+++ script/function_articulos.js
@@ -3,7 +3,6 @@ var articulos; // Variable global
 $.getJSON('script/articulos.json', function(data) {
   articulos = data.articulosRunning;
   generarProductos(articulos, "#container_accesorios");
-  drawChart();
 });
 
 function generarProductos(lista, donde) {
@@ -29,9 +28,7 @@ function generarProductos(lista, donde) {
   document.querySelector(donde).innerHTML = html;
 }
 
-
-
-// Llamar a la función marcascuando se seleccionen los checkboxes
+// Llamar a la función marcas cuando se seleccionen los checkboxes
 function filtro() {
   var checkboxes = document.getElementsByName('marca');
   var marcasSeleccionadas = [];
@@ -75,52 +72,3 @@ function filtro() {
     console.log(articulo.id, articulo.marca, articulo.descripcion, articulo.precio, articulo.color, articulo.genero);
   });
 }
-
-
-
-// --- GOOGLE CHARTS --- //
-
-// function drawChart() {
-//   // Cargar la biblioteca de Google Charts
-//   google.charts.load('current', { packages: ['corechart'] });
-
-//   // Llamar a la función de dibujo después de que la biblioteca se haya cargado
-//   google.charts.setOnLoadCallback(drawChartCallback);
-// }
-
-// // Función de devolución de llamada para dibujar el gráfico
-// function drawChartCallback() {
-//   // Crear un objeto de datos para el gráfico
-//   var data = new google.visualization.DataTable();
-//   data.addColumn('string', 'Marca');
-//   data.addColumn('number', 'Cantidad');
-
-//   // Calcular la cantidad de productos por marca
-//   var marcas = {};
-//   for (var i = 0; i < articulos.length; i++) {
-//     var articulo = articulos[i];
-//     if (marcas[articulo.marca]) {
-//       marcas[articulo.marca]++;
-//     } else {
-//       marcas[articulo.marca] = 1;
-//     }
-//   }
-
-//   // Agregar filas al objeto de datos
-//   for (var marca in marcas) {
-//     data.addRow([marca, marcas[marca]]);
-//   }
-
-//   // Configurar las opciones del gráfico
-//   var options = {
-//     title: 'Cantidad de productos por marca',
-//     width: 400,
-//     height: 300,
-//   };
-
-//   // Crear una instancia del gráfico de barras
-//   var chart = new google.visualization.BarChart(document.getElementById('chart-container'));
-
-//   // Dibujar el gráfico con los datos y opciones especificados
-//   chart.draw(data, options);
-// }
--- script/indice.js
+++ script/indice.js
@@ -1,14 +1,14 @@
-$(document).ready(function() {
- $('#container_bicicletas').load("web/grupo_bicicletas_1.html");
+// $(document).ready(function() {
+//  $('#container_bicicletas').load("web/grupo_bicicletas_1.html");
 
-  $("li > a").click(function(e) {
-    e.preventDefault();
-    var pageUrl = $(this).attr("data-page-url");
-    $("#container_bicicletas").load(pageUrl);
-  });
+//   $("li > a").click(function(e) {
+//     e.preventDefault();
+//     var pageUrl = $(this).attr("data-page-url");
+//     $("#container_bicicletas").load(pageUrl);
+//   });
 
  
-});
+// });
 
 
 
